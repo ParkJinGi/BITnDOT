77916a6fb77813ce9eae7e03e6c311548b74ee76
park <rlqkrwls@naver.com>
2018-08-18 22:53:33 +0900

    API 분리

--- src/header.h
+++ src/header.h
@@ -3,22 +3,10 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
-#include <wiringPi.h>
-#include <wiringShift.h>
-
+#include "queue.h"
 #define MODULE_CNT 7
 
-typedef struct Node {
-	unsigned char data;
-	struct Node *next;
-}Node;
-
-
-typedef struct Queue {
-	Node *front; 	
-	Node *rear; 
-	int count;
-}Queue;
+Queue queue;
 
 int clock_pin[MODULE_CNT] = {29,};
 int latch_pin[MODULE_CNT] = {28,};
@@ -29,28 +17,4 @@ unsigned char SOL_NUM[6] = {
 	0b00010000, 0b00100000, 0b01000000
 };
 
-/*print to console*/
-void print_queue();
 
-/*shut down specific module*/
-void clear(int module_num);
-
-/*shut down every module*/
-void clear_all();
-
-/*control specific module*/
-void control_module(int module_num, unsigned char data);
-
-/*make date using unicode and fill the queue*/
-void decoder(int unicode);
-
-/*check prev unicode is figure*/
-int prev_is_figure();
-
-/***********QUEUE LIBRARY**************/
-void InitQueue(Queue *queue);
-int IsEmpty(Queue *queue); 
-void Enqueue(Queue *queue, unsigned char data); 
-unsigned char Dequeue(Queue *queue); 
-unsigned char Dequeue_Back(Queue *queue);
-/**************************************/
